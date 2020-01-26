90c9b08f46125a2d2f01884ac11950158c507510
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2020-01-25 23:58:02 -0300

    Level 1 - Username and Password Only

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
+const mongoose = require('mongoose')
+
+const app = express()
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+
+mongoose.connect('mongodb://localhost:27017/userDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {console.log('Connected to MongoDB')})
+  .catch((err) => {console.log(err)})
+
+const userSchema = {
+  email: String,
+  password: String
+}
+
+const User = new mongoose.model('User', userSchema)
+
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.get('/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/register', (req, res) => {
+  const { username, password } = req.body
+
+  User.create({
+    email: username,
+    password
+  }, (err) => {
+    if (err) {
+      console.log(err)
+    } res.render('secrets')
+  })
+})
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body
+
+  User.findOne({
+    email: username
+  }, (err, foundUser) => {
+    if (err) {
+      console.log(err)
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render('secrets')
+        }
+      }
+    }
+  })
+})
+
+app.listen(3000, () => {console.log('Server running on port 3000')})
